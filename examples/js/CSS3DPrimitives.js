511f782ec8b2fa20d66c7b45479125b3659714b1
Jamie Kosoy <jkosoy@gmail.com>
2013-08-01 16:23:00 -0400

    Added Pyramid primitive.

--- examples/js/CSS3DPrimitives.js
+++ examples/js/CSS3DPrimitives.js
@@ -31,11 +31,11 @@ THREE.CSS3D.Primitives.Plane = function(w,h) {
 	var domElement = document.createElement("div");
 	var domElement3d = document.createElement("div");
 
-	domElement.classList.add("css3d-plane-container");  
+	domElement.classList.add("three-plane-container");  
 	domElement.style.width = w + "px";
 	domElement.style.height = h + "px";
 
-	domElement3d.classList.add("css3d-plane");
+	domElement3d.classList.add("three-plane");
 	domElement3d.style.width = "100%";
 	domElement3d.style.height = "100%";
 	domElement3d.style.position = "absolute";
@@ -54,16 +54,19 @@ THREE.CSS3D.Primitives.Box = function(w,h,d) {
 	var domElement = document.createElement("div");
 	var domElement3d = document.createElement("div");
 	var sides = ["front","back","right","left","top","bottom"];
-	var figure;
+	var div;
 
-	domElement.classList.add("css3d-box-container");  
+	domElement.classList.add("three-box-container");  
 	domElement.style.width = w + "px";
 	domElement.style.height = h + "px";
 	domElement.style.position = "relative";
 
-	domElement3d.classList.add("css3d-box");
+	domElement3d.classList.add("three-box");
 	domElement3d.style.width = "100%";
 	domElement3d.style.height = "100%";
+	domElement3d.style.margin = "0";
+	domElement3d.style.border = "0";
+	domElement3d.style.padding = "0";	
 	domElement3d.style.position = "absolute";
 	domElement3d.style.WebkitTransformStyle = 'preserve-3d';
 	domElement3d.style.MozTransformStyle = 'preserve-3d';
@@ -76,11 +79,11 @@ THREE.CSS3D.Primitives.Box = function(w,h,d) {
 		var props = {};
 		var transforms = {};
 
-		figure = document.createElement("figure");
-		figure.classList.add("side");
-		figure.classList.add(side);
-		figure.style.display = "block";
-		figure.style.position = "absolute";
+		div = document.createElement("div");
+		div.classList.add("side");
+		div.classList.add(side);
+		div.style.display = "block";
+		div.style.position = "absolute"
 
 		switch(side) {
 			case "front":
@@ -132,17 +135,19 @@ THREE.CSS3D.Primitives.Box = function(w,h,d) {
 
 		var key;
 		for(key in props) {
-			figure.style[key] = props[key];
+			div.style[key] = props[key];
 		}
 
-		figure.style.transform = "";
-		figure.style.webkitTransform = "";
+		div.style.transform = "";
+		div.style.webkitTransform = "";
 		for(key in transforms) {
-			figure.style.transform += key + "(" + transforms[key] + ")";
-			figure.style.webkitTransform += key + "(" + transforms[key] + ")";
+			div.style.transform += key + "(" + transforms[key] + ")";
+			div.style.webkitTransform += key + "(" + transforms[key] + ")";
+			div.style.mozTransform += key + "(" + transforms[key] + ")";
+			div.style.oTransform += key + "(" + transforms[key] + ")";
 		}
 
-		domElement3d.appendChild(figure);
+		domElement3d.appendChild(div);
 	});
 
 	THREE.CSS3D.Primitives.Primitive.call(this,domElement);
@@ -155,7 +160,118 @@ THREE.CSS3D.Primitives.Box.prototype = Object.create( THREE.CSS3D.Primitives.Pri
 */
 THREE.CSS3D.Primitives.Cube = function(size) {
 	THREE.CSS3D.Primitives.Box.call(this,size,size,size);
+
+	this.element.classList.add("three-cube-container");
+	this.element.firstChild.classList.add("three-cube");
 }
 
 THREE.CSS3D.Primitives.Cube.prototype = Object.create( THREE.CSS3D.Primitives.Box.prototype );
 
+/*
+	Pyramid
+*/
+THREE.CSS3D.Primitives.Pyramid = function(size) {
+	var domElement = document.createElement("div");
+	var domElement3d = document.createElement("div");
+	var sides = ["base","front","left","right"];
+	var div;
+
+	var halfSize = size/2;
+	var origin = size/2 + "px " + size/2 + "px " + size/2 + "px";
+	var apex = size * (Math.sqrt(3)/2);
+
+	domElement.classList.add("three-pyramid-container");  
+	domElement.style.WebkitTransformOrigin = origin;
+	domElement.style.MozTransformOrigin = origin;
+	domElement.style.oTransformOrigin = origin;
+	domElement.style.transformOrigin = origin;
+
+	domElement3d.classList.add("three-pyramid");
+	domElement3d.style.width = "0";
+	domElement3d.style.height = "0";
+	domElement3d.style.position = "absolute";
+	domElement3d.style.WebkitTransformStyle = 'preserve-3d';
+	domElement3d.style.MozTransformStyle = 'preserve-3d';
+	domElement3d.style.oTransformStyle = 'preserve-3d';
+	domElement3d.style.transformStyle = 'preserve-3d';
+
+	domElement.appendChild(domElement3d);
+
+	sides.forEach(function(side) {
+		var props = {};
+		var transforms = {};
+
+		div = document.createElement("div");
+		div.classList.add("side");
+		div.classList.add(side);
+
+		div.style.position = "absolute";
+		div.style.width = 0;
+		div.style.height = 0;
+		div.style.margin = 0;
+		div.style.border = 0;
+		div.style.padding = 0;	
+		div.style.WebkitTransformOrigin = "0 0";
+		div.style.MozTransformOrigin = "0 0";
+		div.style.oTransformOrigin = "0 0";
+		div.style.transformOrigin = "0 0";
+	    div.style.borderStyle = "solid";
+	    div.style.borderWidth = (size/2) + "px 0 " + (size/2) + "px " + apex + "px";
+	    div.style.borderColor = "transparent";
+
+		// jk: TO DO
+		switch(side) {
+			case "base":
+			transforms.rotateY = -19.5 + "deg";
+			transforms.rotateX = 180 + "deg";
+			transforms.translateY = -size + "px";
+			break;
+
+			case "front":
+			transforms.rotateY = 90 + "deg";
+			transforms.rotateZ = 60 + "deg";
+			transforms.rotateX = 180 + "deg";
+			transforms.translateY = -size + "px";
+			break;
+
+			case "left":
+			transforms.rotateX = 60 + "deg";
+			transforms.rotateY = 19.5 + "deg";
+			break;
+
+			case "right":
+			var x = (size/100) * -29;
+			var y = (size/100) * -50;
+			var z = (size/100) * 81.5;
+
+      		transforms.rotateX = -60 + "deg";
+      		transforms.rotateY = 19.5 + "deg";
+      		transforms.translateX = x + "px";
+      		transforms.translateY = y + "px";
+      		transforms.translateZ = z + "px";
+
+			break;
+		}
+
+
+		var key;
+		for(key in props) {
+			div.style[key] = props[key];
+		}
+
+		div.style.transform = "";
+		div.style.webkitTransform = "";
+		for(key in transforms) {
+			div.style.transform += key + "(" + transforms[key] + ")";
+			div.style.webkitTransform += key + "(" + transforms[key] + ")";
+			div.style.mozTransform += key + "(" + transforms[key] + ")";
+			div.style.oTransform += key + "(" + transforms[key] + ")";
+		}
+
+		domElement3d.appendChild(div);
+	});
+
+	THREE.CSS3D.Primitives.Primitive.call(this,domElement);
+}
+
+THREE.CSS3D.Primitives.Pyramid.prototype = Object.create( THREE.CSS3D.Primitives.Primitive.prototype );
